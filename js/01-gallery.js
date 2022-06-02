69f3107be7f8abd765d95b133353fcab96c85d88
Evgeniy Frolov <95540052+EvgeniyTIR@users.noreply.github.com>
2022-06-02 18:12:03 +0300

    hw-07-js-task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,88 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
 
-console.log(galleryItems);
+    CreateGalleryItems(galleryItems);
+
+    function CreateGalleryItems(obj){
+      const gallery = [];
+
+      for (const el of obj) {
+        const itemConteiner = document.createElement('div')
+        itemConteiner.classList ='gallery__item';
+
+        const itemLink = document.createElement('a');
+        itemLink.classList ='gallery__link';
+        itemLink.href = el.original;
+
+        const itemImg = document.createElement('img');
+        itemImg.src = el.preview;
+        itemImg.dataset.source = el.original;
+        itemImg.alt = el.description;
+        itemImg.classList = 'gallery__image';
+
+        itemConteiner.appendChild(itemLink);  
+        itemLink.appendChild(itemImg); 
+        
+        gallery.push(itemConteiner);       
+      }; 
+      galleryContainer.append(...gallery);   
+    };
+
+
+function OnImageClick(evt) {
+    evt.preventDefault();
+    if(evt.target.nodeName !== 'IMG'){
+        return
+        
+    }else{        
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`)
+   instance.show()
+
+   window.addEventListener("keydown", (evt) => {
+    if (evt.code !== "Escape") {
+      return;
+    }
+    instance.close();
+  });
+    };
+  };
+ 
+
+  galleryContainer.addEventListener('click', OnImageClick); 
+
+// galleryItems.forEach((galleryItem) => {
+//   galleryContainer.insertAdjacentHTML(
+//     "beforeend",
+//     `
+//     <div class="gallery__item">
+//       <a class="gallery__link" href="large-image.jpg">
+//         <img
+//           class="gallery__image"
+//           src=${galleryItem.preview}
+//           data-source=${galleryItem.original}
+//           alt=${galleryItem.description}
+//         />
+//       </a>
+//     </div>`
+//   );
+// });
+// galleryContainer.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   if (e.target.nodeName !== "IMG") {
+//     return;
+//   }
+//   const instance = basicLightbox.create(`
+//     <img src=${e.target.dataset.source} width="800" height="600">
+// `);
+//   instance.show();
+
+//   window.addEventListener("keydown", (e) => {
+//     if (e.code !== "Escape") {
+//       return;
+//     }
+//     instance.close();
+//   });
+// });
