71ac54e1df294a2048671632f5292b6457ee6463
Evgeniy Frolov <95540052+EvgeniyTIR@users.noreply.github.com>
2022-06-03 16:57:54 +0300

    js-hw-07

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,31 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+galleryItems.forEach((galleryItem) => {
+  galleryContainer.insertAdjacentHTML(
+    "beforeend",
+    `    
+      <a class="gallery__link"
+       href="${galleryItem.original}"            
+       >
+        <img
+          class="gallery__image"
+          src=${galleryItem.preview}
+          data-source=${galleryItem.original}                    
+          alt="${galleryItem.description}"
+        />
+      </a>
+    `
+  );
+}); 
+
+var lightbox = new SimpleLightbox('.gallery a', 
+   {maxZoom: 3,
+    captions: true,
+    captionSelector: "img",
+    captionType: "attr",
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,});
